320b15867b1ff387e63f88d6f2364839c72a5e7a
andysingal <andysingal@gmail.com>
2023-04-09 20:33:00 +0530

    oops project-11

--- oop_project_2/app.py
+++ oop_project_2/app.py
@@ -1,5 +1,5 @@
 
-
+student_list = []
 def create_student():
     name = input("Please enter the new student's name: ")  #Ask the user for the student's name
     student_data = {
@@ -28,3 +28,20 @@ def print_student_list(students):
     for i, student in enumerate(students):
         print("ID: {}".format(i))
         print_student_details(student)
+
+def menu():
+    selection = input("Enter 'p' to print the student list, "
+                      "'s' to add a new student, "
+                      "'a' to add a mark to a student, "
+                      "or 'q' to quit. "
+                      "Enter your selection: ")
+    while selection != 'q':
+        if selection == 'p':
+            print_student_list(student_list)
+        elif selection == 's':
+            student_list.append(create_student())
+        elif selection == 'a':
+            student_id = int(input("Enter the student ID to add a mark to: "))
+            student = student_list[student_id]
+            new_mark = int(input("Enter the new mark to be added: "))
+            add_mark(student, new_mark)
