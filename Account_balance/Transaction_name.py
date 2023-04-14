90f1a3e35a7417357ddca61f74818865cd81ad4d
andysingal <andysingal@gmail.com>
2023-04-14 10:51:32 +0530

    oops projects

--- Account_balance/Transaction_name.py
+++ Account_balance/Transaction_name.py
@@ -4,8 +4,9 @@ import numbers
 
 class Account:
     transaction_counter = itertools.count(100)
+    interest_rate = 0.5  # percentage
 
-    def __init__(self, account_number,first_name,last_name, timezone=None):
+    def __init__(self, account_number,first_name,last_name, timezone=None,initial_balance=0):
         self._account_number = account_number
         self._first_name = first_name
         self.last_name = last_name
@@ -13,6 +14,8 @@ class Account:
         if timezone is None:
             timezone = TimeZone('UTC', 0, 0)
         self.timezone = timezone
+
+        self._balance = float(initial_balance)  # force use of floats here, but maybe Decimal would be better
 
     @property
     def account_number(self):
@@ -43,11 +46,27 @@ class Account:
     def timezone(self):
         return self._timezone
 
+    @property
+    def balance(self):
+        return self._balance
+
     @timezone.setter
     def timezone(self, value):
         if not isinstance(value, TimeZone):
             raise ValueError('Time zone must be a valid TimeZone object.')
         self._timezone = value
+
+    @classmethod
+    def get_interest_rate(cls):
+        return cls._interest_rate
+
+    @classmethod
+    def set_interest_rate(cls, value):
+        if not isinstance(value, numbers.Real):
+            raise ValueError('Interest rate must be a real number')
+        if value < 0:
+            raise ValueError('Interest rate cannot be negative.')
+        cls._interest_rate = value
 
     def validate_and_set_name(self, property_name, value, field_title):
         if value is None or len(str(value).strip()) == 0:
@@ -62,13 +81,21 @@ class Account:
 #
 # a = Account('12345', 'Alex', 'Martelli')
 # print(a.first_name, a.last_name, a.full_name)
+#
+# try:
+#     a = Account('123', 'John', 'Smith',  initial_balance=100)
+# except ValueError as ex:
+#     print(ex)
+#
+# a = Account('123', 'John', 'Smith')
+# print(a.timezone)
+a1 = Account(1234, 'Monty', 'Python', initial_balance=0)
+a2 = Account(2345, 'John', 'Cleese', initial_balance=0)
+print(a1.interest_rate, a2.interest_rate)
 
 try:
-    a = Account('123', 'John', 'Smith', '-7:00')
-except ValueError as ex:
-    print(ex)
-
-a = Account('123', 'John', 'Smith')
-print(a.timezone)
+    a1.balance = 200
+except AttributeError as ex:
+        print(ex)
 
 
